60cc9dcbe44993566b6487352c85a309244b8ce5
EDAO-TECH <INFO@NEVERMISSEDLICENSEDTRUST.TECH>
2025-07-19 01:06:00 +0000

    Enable SVGR support for inline SVG components

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const HOSTNAME = process.env.GITHUB_CODESPACE_HOSTNAME
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    cors: true,
-    hmr: {
-      protocol: 'wss',
-      host: HOSTNAME,
-    },
-    origin: `https://${HOSTNAME}`,
-  },
-})
+  plugins: [react(), svgr()],
+});
